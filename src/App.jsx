2e8ab54a6347c22f223c9bee08e21b73b0d09a68
gillyhuga <gilly.huga@yahoo.co.id>
2024-05-07 22:33:52 +0700

    chore: install i18next

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t } = useTranslation();
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">
+        {t("headerTitle")}
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
